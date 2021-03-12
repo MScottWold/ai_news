36e56ac75995f94d5b54fcedab26b6b532ebdc55
mitchell <wold.ms@gmail.com>
2021-03-12 01:45:47 -0800

    Add user dropdown menu
    Added user dropdown menu and changed the favorite.svg to include the
    user avatar image.

--- app/javascript/components/navigation/greeting.jsx
+++ app/javascript/components/navigation/greeting.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Greeting = ({ loggedIn, username, showModal, logout }) => {
-  if (loggedIn) {
+  if (!loggedIn) {
     return (
-      <div className="greeting">
-        <p>Welcome, {username}!</p>
-        <a href="#/favorites">favorites</a>
-        <button onClick={logout}>
-          log out
-        </button>
+      <div id="login-buttons">
+        <button className="session-button" onClick={() => showModal('login')}>log in</button>
+        <button className="session-button" onClick={() => showModal('signup')}>sign up</button>
       </div>
     );
   }
 
+  const [dropdown, setDropdown ] = useState(false);
+
+  const menu = dropdown ? (
+    <ul className="dropdown" 
+      onClick={() => setDropdown(false)}>
+      <li className="menu-item"
+        onClick={() => window.location.hash = '/favorites'}
+        >favorites</li>
+      <li className="menu-item" onClick={logout}>log out</li>
+    </ul>
+  ) : (
+    null
+  );
+  
   return (
-    <div className="greeting">
-      <button className="session-button" onClick={() => showModal('login')}>log in</button>
-      <button className="session-button" onClick={() => showModal('signup')}>sign up</button>
+    <div className="greeting" onMouseLeave={() => setDropdown(false)}>
+      <div>{username}</div>
+      <div
+        id="avatar"
+        onMouseEnter={() => setDropdown(true)}>
+      </div>
+      {menu}
     </div>
   );
 };
